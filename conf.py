85c03b5e63e1c16adb33ac474a85f4fe667474d2
Adrian Webb <adrian@webb.sh>
2020-04-26 06:59:19 +0200

    Adding an initial Sphinx configuration.

---
+++ conf.py
@@ -0,0 +1,64 @@
+import os
+import sphinx_rtd_theme
+
+DOCS_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(DOCS_DIR)
+VERSION_PY_PATH = os.path.join(BASE_DIR, 'version.py')
+
+# -- General configuration ------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode'
+]
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = 'DCCS US Draft Bylaws'
+copyright = '2020, DCCS Corporation'
+author = 'dccs.tech'
+
+_globs = {}
+exec(open(VERSION_PY_PATH).read(), _globs)
+version = _globs['VERSION']
+release = version
+del _globs
+
+language = 'en'
+exclude_patterns = ['_build']
+pygments_style = 'default'
+todo_include_todos = True
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'style_nav_header_background': '#021026',
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+templates_path = ['_templates']
+html_static_path = ['_static']
+html_logo = "_static/DCCS.png"
+html_favicon = "_static/favicon.ico"
+
+html_show_sourcelink = True
+html_use_index = True
+html_split_index = True
+
+# -- General configuration ------------------------------------------------
+
+def setup(app):
+    app.add_css_file('_css/override.css')
